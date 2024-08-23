eaf131ecc7925179b7c634c70555c58a440fda25
Phellype <phellypeaugustho@gmail.com>
2024-08-23 11:47:59 -0300

    atualizacao02

---
+++ tipos-variaveis/src/Operadores.java
@@ -0,0 +1,90 @@
+public class Operadores {
+
+    public static void main(String[] args) {
+        // String concatenacao = "?";
+
+        // concatenacao = 1 + 1 + 1 + "1";
+
+        // System.out.println(concatenacao);
+
+        // concatenacao = 1 + "1" + 1 + 1;
+
+        // System.out.println(concatenacao);
+
+        // concatenacao = 1 + "1" + 1 + 1 + "1";
+
+        // System.out.println(concatenacao);
+
+        // concatenacao = "1" + 1 + 1 + "1";
+
+        // System.out.println(concatenacao);
+
+        // concatenacao = "1" + (1 + 1 + 1);
+
+        // System.out.println(concatenacao);
+        // -------------------------------------------------------------------------
+        // int numero = 5;
+        // numero = -numero;
+        // System.out.println(numero);
+        // numero = numero * -1;
+        // System.out.println(numero);
+        // -------------------------------------------------------------------------
+        // int numero = 5;
+
+        // numero++;
+
+        // System.out.println(numero);
+
+        // boolean variavel = true;
+
+        // System.out.println(!variavel);
+        // -------------------------------------------------------------------------
+
+        // int a, b;
+
+        // a = 6;
+        // b = 6;
+
+        // String resultado = a==b ? " Verdadeiro" : "Falso";
+        // // if(a==b){
+        // // resultado = "Verdadeiro";
+        // // }else{
+        // // resultado = "Falso";
+        // // }
+
+        // System.out.println(resultado);
+
+        // int numero1 = 1;
+        // int numero2 = 2;
+
+        // boolean simNao = numero1 == numero2;
+
+        // if(numero1<numero2){
+        // System.out.println("a nossa condicao e verdadeira");
+        // }else{
+        // System.out.println("a nossa condicao e falsa");
+        // }
+
+        // System.out.println("numeroUm é igual o numeroDois" + simNao);
+
+        // simNao = numero1 != numero2;
+
+        // System.out.println("numeroUm é diferente o numeroDois" + simNao);
+
+        // simNao = numero1 > numero2;
+
+        // System.out.println("numeroUm é maior que o numeroDois" + simNao);
+
+        boolean condicao1 = true;
+        boolean condicao2 = false;
+
+        if (condicao1 && condicao2) {
+            System.out.println("As duas condicoes sao verdadeiras");
+        } else if (condicao1 || condicao2) {
+            System.out.println("Uma duas condicoes é verdadeiras");
+        }
+        System.out.println("Fim");
+
+    }
+
+}
